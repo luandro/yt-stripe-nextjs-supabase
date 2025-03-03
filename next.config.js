46b13ca81d8a0f8f6e39b713546fc2971c3ab26d
luandro <luandro@digital-democracy.org>
2025-03-02 21:47:16 -0300

    Add Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // ... your existing config ...
+  output: 'standalone',
   webpack: (config, { isServer }) => {
     config.ignoreWarnings = [
       { module: /node_modules\/punycode/ }
